fed48463e83a0a7405357f61100f14ab7776c63b
sharvil1234 <71960080+sharvil1234@users.noreply.github.com>
2021-05-31 12:26:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+song = "";
+
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized");
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);    
+}
+
+function play()
+{
+    song.play();
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
+
+    }
+}
